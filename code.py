26571cd64936918fa8bd0371d5f012f3b82738de
Bart Schapendonk <bart@schapendonk.com>
2021-11-28 12:54:23 +0100

    wifi test

--- code.py
+++ code.py
@@ -1,1 +1,23 @@
-print("Hello World!")
+import time
+import ssl
+import socketpool
+import wifi
+
+try:
+    from secrets import secrets
+except ImportError:
+    print("WiFi secrets are kept in secrets.py, please add them there!")
+    raise
+
+print("My MAC addr:", [hex(i) for i in wifi.radio.mac_address])
+
+print("Available WiFi networks:")
+for network in wifi.radio.start_scanning_networks():
+    print("\t%s\t\tRSSI: %d\tChannel: %d" % (str(network.ssid, "utf-8"),
+            network.rssi, network.channel))
+wifi.radio.stop_scanning_networks()
+
+print("Connecting to %s"%secrets["ssid"])
+wifi.radio.connect(secrets["ssid"], secrets["password"])
+print("Connected to %s!"%secrets["ssid"])
+print("My IP address is", wifi.radio.ipv4_address)
